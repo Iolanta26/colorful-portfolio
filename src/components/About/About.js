f10854687d4e49990075f65a9654cc9fb9d425ed
Iolanta26 <iolanta.u@gmail.com>
2022-02-09 19:14:38 +0200

    about small animation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,33 +21,33 @@ const About = () => {
 
   return (
     <div className="about" id="about">
-      <div className="aboutAnimation">
-        <div className="lying" ref={container}></div>
-        <div className="btn-shine">About</div>
-      </div>
+      <div className="btn-shine">About</div>
       <div className="aboutContainer">
-        <div className="imgButton">
+        <div className="image-text-container">
           <img src={me} alt="me" className="me" />
+          <div className="aboutText">
+            <p>
+              Frontend is my passion. I got interested in Software Development
+              in early 2019.
+              <br />
+              <br />I have completed my Full Stack Web Development studies at
+              Business College Helsinki and am looking for a Junior Frontend
+              Developer job. <br />
+              <br />
+              I work mainly with Javascript and React. And I have a huge
+              experience in digital design and various related apps.
+              <br />
+              <br />
+              My skills and goal-oriented character can make magic at the
+              working place. Do not hesitate to call me for the interview.
+            </p>
+          </div>
+        </div>
+        <div className="btnAnimation">
+          <div className="lying" ref={container}></div>
           <a href={pdf} download="Iolanta_Scheifel" className="btn">
             Download CV
           </a>
-        </div>
-        <div className="aboutText">
-          <p>
-            Frontend is my passion. I got interested in Software Development in
-            early 2019.
-            <br />
-            <br />I have completed my Full Stack Web Development studies at
-            Business College Helsinki and am looking for a Junior Frontend
-            Developer job. <br />
-            <br />
-            I work mainly with Javascript and React. And I have a huge
-            experience in digital design and various related apps.
-            <br />
-            <br />
-            My skills and goal-oriented character can make magic at the working
-            place. Do not hesitate to call me for the interview.
-          </p>
         </div>
       </div>
       <div className="sunContainer">
