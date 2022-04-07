339fb3a5f0380f016e7ae2a9a3eb061695a71f2d
Iolanta26 <iolanta.u@gmail.com>
2022-04-07 11:44:29 +0200

    copy button enabled

---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import "./contacts.css";
+
+import email from "./email.svg";
+import phone from "./phone.svg";
+
+const Contacts = () => {
+  const [emailCopied, setEmailCopied] = useState();
+  const [phoneCopied, setPhoneCopied] = useState();
+
+  useEffect(() => {
+    navigator.clipboard.writeText(emailCopied);
+  }, [emailCopied]);
+
+  useEffect(() => {
+    navigator.clipboard.writeText(phoneCopied);
+  }, [phoneCopied]);
+
+  return (
+    <div className="contacts" id="contacts">
+      <h1 className="contact-name">Let's talk</h1>
+      <div className="contact-info-container">
+        <div className="contact-info-wrapper">
+          <div className="contact-mail">
+            <img className="mail" src={email} alt="" />
+            <p className="info">iolanta.scheifel@gmail.com</p>
+          </div>
+          <div className="contact-phone">
+            <img className="phone" src={phone} alt="" />
+            <p className="info">+3584456000</p>
+          </div>
+        </div>
+        <div className="copy-btns">
+          <button
+            className="copy-button"
+            disabled={emailCopied}
+            onClick={() => setEmailCopied("iolanta.scheifel@gmail.com")}
+          >
+            {emailCopied ? "Copied" : "Copy"}
+          </button>
+          <button
+            className="copy-button"
+            disabled={phoneCopied}
+            onClick={() => setPhoneCopied("+3584456000")}
+          >
+            {phoneCopied ? "Copied" : "Copy"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
