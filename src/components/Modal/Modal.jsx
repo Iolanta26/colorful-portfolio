4ff87b2eea25d484407d70b07b1c756982b7b7e9
iolantascheifel <iolanta.scheifel@gmail.com>
2022-10-25 22:18:34 +0200

    modal bug fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,20 +6,18 @@ import Skills from "../Skills/Skills";
 
 const Modal = ({ closeModal }) => {
   return (
-    <div className="background">
-      <div className="modal-container">
-        <div className="info-wrapper">
-          <Player
-            autoplay
-            loop
-            src={animation}
-            style={{ height: "150px", width: "150px" }}
-          />
-          <button className="close-btn" onClick={() => closeModal(false)}>
-            <img src={x} alt="x" />
-          </button>
-          <Skills />
-        </div>
+    <div className="modal-container">
+      <div className="info-wrapper">
+        <Player
+          autoplay
+          loop
+          src={animation}
+          style={{ height: "150px", width: "150px" }}
+        />
+        <button className="close-btn" onClick={() => closeModal(false)}>
+          <img src={x} alt="x" />
+        </button>
+        <Skills />
       </div>
     </div>
   );
